800dd6882746cec8a76ab9804ba9018cfcca467a
秀和 <hidekazu5200@gmail.com>
2018-04-03 05:41:43 +0800

    dbconnect.php、home.phpの追加

---
+++ dbconnect.php
@@ -0,0 +1,8 @@
+<?php  $dsn = 'mysql:dbname=kami;host=localhost';
+ // XAMPP環境下ではユーザー名rootパスワード空
+ $user = 'root';
+ $password = '';
+ //このプログラムが存在している場所と同じサーバーを指定
+ $dbh = new PDO($dsn,$user,$password);
+ $dbh->query('SET NAMES utf8');
+ ?>
